fc78cbfc525bd424005e31cc3e106d37b34c1a81
Megan (AKA Pilahi) Moran <kamokumoran@gmail.com>
2020-08-27 18:25:20 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -6,9 +6,9 @@ class Student
   attr_reader :id
   
   def initialize(id=nil, name, grade)
-    @name = name
+    @id = id@name = name
     @grade = grade
-    @id = id
+    
   end 
   
   def self.create_table
