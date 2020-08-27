1679918b422bc435d5ebdb8885ed82c8ed785cc1
Megan (AKA Pilahi) Moran <kamokumoran@gmail.com>
2020-08-27 18:13:15 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,8 +2,10 @@ require_relative "../config/environment.rb"
 
 class Student
 
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]
-
+  attr_accessor :name, :grade
+  attr_reader :id
+  
+  def 
+  end 
 
 end
