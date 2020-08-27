eb6a7ba540988ec28fa9e8497e51a2c60fa39edb
Megan (AKA Pilahi) Moran <kamokumoran@gmail.com>
2020-08-27 18:19:24 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -52,6 +52,12 @@ class Student
   end
   
   def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT * FROM students 
+      WHERE name = ? 
+      LIMIT 1
+    SQL
+    
   end
   
   def self.update
