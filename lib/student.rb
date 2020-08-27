0ee5433989d3125fc0bfab074927e0e494e37f2c
Megan (AKA Pilahi) Moran <kamokumoran@gmail.com>
2020-08-27 18:17:06 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -23,9 +23,15 @@ class Student
   end
   
   def self.drop_table
+    sql = <<-SQL
+      DROP TABLE students
+    SQL
+    
+    DB[:conn].execute(sql)
   end
   
   def self.save
+    
   end
   
   def self.create
